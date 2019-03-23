f903c23a14c60fd1d8a1342a6c91cb18ed696fab
horangijk <iamjooyoung@gmail.com>
2019-03-23 17:36:38 -0400

    Fixed jquery error

--- app/assets/javascripts/players.js
+++ app/assets/javascripts/players.js
@@ -1,11 +1,7 @@
-$(() => {
-  bindClickHandlers()
+$(document).on('turbolinks:load', function(){
+
+      $('.all_players').on('click', function(e) {
+        e.preventDefault()
+        console.log($('.all_players'))})
+
 })
-
-
-const bindClickHandlers = () => {
-    $('.all_players').on('click', (e) => {
-      e.preventDefault()
-      console.log('hello')
-    })
-}
